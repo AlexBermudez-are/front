a7a0ef6ace47d5cde41b56d0009832381749af22
Alexis Bermúdez <71720889+AlexBermudez-are@users.noreply.github.com>
2024-05-23 10:02:45 -0300

    primer y ultimo push

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { setAxiosDefaults } from './utils/axiosConfig';
 
+setAxiosDefaults()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
